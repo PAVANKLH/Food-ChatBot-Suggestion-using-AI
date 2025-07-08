4c0aa7c49454eefb8b2ff7e4674ad01f2ba0851b
varun <varunmudiraj154@gmail.com>
2025-07-08 18:23:36 +0530

    Fix for Render deployment: use PORT env and bind to 0.0.0.0

--- app.py
+++ app.py
@@ -363,7 +363,10 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    with app.app_context():
+        db.create_all()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 # import os
